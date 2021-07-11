9a21a05d70402a910dc749a20f73b881574adf5f
Bobby <bobbydura10@gmail.com>
2021-07-11 18:23:22 +0545

    comimit

--- src/Components/Covid.js
+++ src/Components/Covid.js
@@ -4,28 +4,38 @@ import EventAvailableIcon from '@material-ui/icons/EventAvailable';
 import AcUnitIcon from '@material-ui/icons/AcUnit';
 import AccessibilityNewIcon from '@material-ui/icons/AccessibilityNew';
 import PersonAddDisabledIcon from '@material-ui/icons/PersonAddDisabled';
+import AccessTimeIcon from '@material-ui/icons/AccessTime';
 
 
 //For greeting
 let currDate = new Date().getHours();
 let greeting = "";
 
+
 if (currDate >= 1 && currDate < 12) {
     greeting = "Good Morning ! ";
 }
-else if (currDate <= 12 && currDate < 19) {
+else if (currDate >= 12 && currDate < 20) {
     greeting = "Good Evening !";
 }
 else {
     greeting = " Good Night ! ";
 }
 
-let time = new Date().toLocaleDateString();
+let newdate = new Date().toLocaleDateString(); //for date
+let digitime = new Date().toLocaleTimeString(); //for digital clock time
 
 //fetching api // used react hook (usestate,useeffect)
 const Covid = () => {
     const [data, setData] = useState([]);
-   
+    const [ctime, settime] = useState(digitime);
+
+    const updatetime = () => {
+        digitime = new Date().toLocaleTimeString();
+        settime(digitime);
+    }
+    setInterval(updatetime, 1000);
+
     const getCovidData = async () => {
         try {
             const res = await fetch('https://api.covid19api.com/summary');
@@ -45,12 +55,13 @@ const Covid = () => {
         <h3> Corona Virus 🦠 Tracker🤒  </h3>
         <ul>
             <li className="main">
-                <div className="main_inner">   <h4> Nepal's Total Cases</h4>
-                    <strong className="date_cs"><EventAvailableIcon className="date_icon" />&nbsp; &nbsp;&nbsp;{time}</strong>
+                <div className="main_inner">   <h4> Nepal Overview</h4>
+                    <strong className="date_cs"><EventAvailableIcon className="date_icon" />{newdate}</strong>
+                    <strong className="date_cs"><AccessTimeIcon className="time_icon" />{ctime}</strong>
                     <div className="row"  >
-                        <p className="main_total"> <span>< AcUnitIcon className="confirm_icon" />&nbsp;Total Confirmed:&nbsp;{data.TotalConfirmed}</span></p>
-                        <p className="main_total"> <span><AccessibilityNewIcon className="recover_icon" />&nbsp;Total Recovered:&nbsp;{data.TotalRecovered}</span></p>
-                        <p className="main_total"> <span><PersonAddDisabledIcon className="death_icon" />&nbsp;   Total Deaths: &nbsp;{data.TotalDeaths}</span></p>
+                        <p className="main_total"> <span>< AcUnitIcon className="confirm_icon" />Total Confirmed: {data.TotalConfirmed}</span></p>
+                        <p className="main_total"> <span><AccessibilityNewIcon className="recover_icon" />Total Recovered: {data.TotalRecovered}</span></p>
+                        <p className="main_total"> <span><PersonAddDisabledIcon className="death_icon" /> Total Deaths : {data.TotalDeaths}</span></p>
                     </div>
                 </div>
             </li>
